e5d8cd70c2d28a3d5f541139e8d6bfae5c82035d
Reymond Bolambao <trd.rbolambao@gmail.com>
2017-07-07 18:52:14 +0800

    [update] add switch button state

--- happ/RegisterController.swift
+++ happ/RegisterController.swift
@@ -11,11 +11,20 @@ import UIKit
 class RegisterController: UIViewController {
 
     
-    /*setting up the variable...*/
+    /*setting up the UI variable...*/
     @IBOutlet var txtEmail: UITextField!
     @IBOutlet var txtpass: UITextField!
     @IBOutlet var txtrepeatpas: UITextField!
     
+    @IBOutlet var frontEndSwitch: UISwitch!
+    @IBOutlet var backEndSwitch: UISwitch!
+    @IBOutlet var iosSwitch: UISwitch!
+    
+    /*setting up varaibles*/
+    var setState: String = ""
+    var frontEndState: String = ""
+    var backEndState: String = ""
+    var IOSstate: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +48,9 @@ class RegisterController: UIViewController {
         let userEmail = txtEmail.text!
         let userPass  = txtpass.text!
         let userRepeatPass = txtrepeatpas.text!
+        let fronEndSkill = getFrontEndState()
+        let backEndSkill = getBackEndState()
+        let iosSkill = getIOSState()
         
         //check empty fields..
         if userEmail == "" || userPass == "" || userRepeatPass == "" {
@@ -47,12 +59,19 @@ class RegisterController: UIViewController {
         else if userPass != userRepeatPass {
             displayMyAlertMessage("Password does not match!")
         } else {
+            
+            print(fronEndSkill)
+            print(backEndSkill)
+            print(iosSkill)
             //save it locally instiate NSUDefaults..
             let defaults = NSUserDefaults.standardUserDefaults()
             
             //store data..
             defaults.setObject(userEmail, forKey: "username")
             defaults.setObject(userPass, forKey: "password")
+            defaults.setObject(fronEndSkill, forKey: "Front_end")
+            defaults.setObject(backEndSkill, forKey: "Back_end")
+            defaults.setObject(iosSkill, forKey: "Ios_end")
             defaults.synchronize()
             
             //show pop up box successfully registered..
@@ -69,7 +88,44 @@ class RegisterController: UIViewController {
         }
         
     }
+  
+    //switch button action
+    @IBAction func frontEndSwitch(sender: UISwitch) {
+        self.frontEndState = clickSwitchButton(frontEndSwitch)
+        
+    }
+    @IBAction func backEndSwitch(sender: UISwitch) {
+        self.backEndState = clickSwitchButton(backEndSwitch)
+    }
+    @IBAction func iosSwitch(sender: UISwitch) {
+        self.IOSstate = clickSwitchButton(iosSwitch)
+    }
 
+    //check button state...
+    func clickSwitchButton(switchButton : UISwitch) -> String {
+        var SwitchState = ""
+        
+        if switchButton.on {
+            SwitchState = "On"
+        }else {
+           SwitchState = "Off"
+        }
+        return SwitchState
+    }
+    
+    //getters...
+    func getFrontEndState() -> String {
+        return self.frontEndState
+    }
+    func getBackEndState() -> String {
+        return self.frontEndState
+    }
+    
+    func getIOSState() -> String {
+        return self.IOSstate
+    }
+    
+    //display aler message box...
     func displayMyAlertMessage(userMessage:String){
         let myAlert = UIAlertController(title: "Happ Message", message: userMessage, preferredStyle: UIAlertControllerStyle.Alert)
         let okAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil)
