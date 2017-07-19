5c7e56baa61715d0b8c0cee12fe6f4ca0e29662e
Reymond Bolambao <trd.rbolambao@gmail.com>
2017-07-19 17:20:25 +0800

    [update] add send resetting email

--- happ/ChangePassViewController.swift
+++ happ/ChangePassViewController.swift
@@ -8,24 +8,68 @@
 
 import UIKit
 
-class ChangePassViewController: UIViewController {
+class ChangePassViewController: UIViewController, UITextFieldDelegate {
     
-//    @IBOutlet var userPass: UITextField!
-//    @IBOutlet var userEmail: UITextField!
-//
-//    @IBOutlet var ExitButtonTapped: UIButton!
-//    @IBOutlet var ChangePassButtonTapped: UIButton!
+    //setting up variable...
+    @IBOutlet var navBack: UIBarButtonItem!
+    @IBOutlet var navTitle: UINavigationItem!
+    @IBOutlet var userEmailField: UITextField!
+    @IBOutlet var labelUserEmail: UILabel!
+    @IBOutlet var userNote: UILabel!
+    
+    @IBOutlet var btnChangePass: UIButton!
+    
+    var changePassparam = [
+        "en": [
+            "navTitle": "Send resetting e-mail",
+            "navBack": "Back",
+            "emailLabel": "Mail address",
+            "emailPlaceholder": "example@xxx.com",
+            "userNote": "Please Enter a Registered Email Address!",
+            "changePassbutton": "Issue reconfiguration URL"
+        ],
+        
+        "ja" : [
+            "navTitle": "再設定メール送信",
+            "navBack": "バック",
+            "emailLabel": "メールアドレス",
+            "emailPlaceholder": "example@xxx.com",
+            "userNote": "登録されたメールアドレスを入力してください",
+            "changePassbutton": "再設定用URLを発行する"
+        ]
+    ]
     
     
-    
-    @IBOutlet var userEmail: UITextField!
-    @IBOutlet var ChangePassButtonTapped: UIButton!
-    @IBOutlet var userPass: UITextField!
+    //var for global language..
+    var language: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //texfield delegate
+        userEmailField.delegate = self
+        
+        //get system language...
+        let systemLang =  NSLocale.currentLocale().objectForKey(NSLocaleLanguageCode)!
+        if systemLang as! String == "ja" {
+            language = systemLang as! String
+        }else{
+            language = "en"
+        }
+        
+        //set text field text and value
+        navTitle.title = changePassparam["\(language)"]!["navTitle"]
+        navBack.title  = changePassparam["\(language)"]!["navBack"]
+        labelUserEmail.text = changePassparam["\(language)"]!["emailLabel"]
+        userEmailField.placeholder = changePassparam["\(language)"]!["emailPlaceholder"]
+        userNote.text = changePassparam["\(language)"]!["userNote"]
+        
+        //set button text..
+        btnChangePass.setTitle(changePassparam["\(language)"]!["changePassbutton"], forState: .Normal)
+        
+        //add listener action to changepass button
+        btnChangePass.addTarget(self, action: "changePass:", forControlEvents: .TouchUpInside)
+       
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,37 +77,111 @@ class ChangePassViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func ExitTapped(sender: UIButton) {
-         self.dismissViewControllerAnimated(true, completion: nil)
-    }
-    @IBAction func ChangePassButtonTapped(sender: AnyObject) {
-        let username = userEmail.text!
-        let password = userPass.text!
-        checkEmail(username
-                    , userPassword: password)
-
-    }
-
-    //check what email password to update
-    func checkEmail(username: String, userPassword: String) {
+    
+    func changePass(sender: AnyObject) {
+        let userEmail = userEmailField.text!
         
-        let defaults = NSUserDefaults.standardUserDefaults()
-        let userEmail = defaults.stringForKey("username")
-        
-        if(userEmail == username) {
-            defaults.removeObjectForKey("password")
-            defaults.setObject(userPassword, forKey: "password")
-            defaults.synchronize()
-            displayMyAlertMessage("Successfully Change Password!")
+        if userEmail == "" {
+            displayMyAlertMessage("Enter your Email")
         } else {
-            displayMyAlertMessage("Not equal username.Please check email!")
+            let changePassUrl = "http://happ.timeriverdesign.com/wp-admin/admin-ajax.php"
+            
+            //created NSURL
+            let requestURL = NSURL(string: changePassUrl)
+            
+            //creating NSMutableURLRequest
+            let request = NSMutableURLRequest(URL: requestURL!)
+            
+            //set boundary string..
+            let boundary = generateBoundaryString()
+            
+            //set value for image upload
+            request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+            
+            //setting the method to post
+            request.HTTPMethod = "POST"
+        
+            
+            if language == "ja" {
+                language = "jp"
+            }
+            //set parameters...
+            let param = [
+                "sercret"     : "jo8nefamehisd",
+                "action"      : "api",
+                "ac"          : "user_rest_pw",
+                "d"           : "1",
+                "lang"        : "\(language)",
+                "user_id"     : "",
+                "email"       : "\(userEmail)"
+            ]
+            
+            
+            //adding the parameters to request body
+            request.HTTPBody = createBodyWithParameters(param, boundary: boundary)
+            
+            let task = NSURLSession.sharedSession().dataTaskWithRequest(request){
+                data, response, error  in
+                
+                var mess: String = ""
+                if error != nil{
+                    print("\(error)")
+                    return;
+                }
+                do {
+                    let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments) as? NSDictionary
+                    if json!["message"] != nil {
+                        mess = json!["message"] as! String
+                    }
+                    if json!["result"] != nil {
+                        if json!["result"]!["mess"] != nil {
+                            mess = json!["result"]!["mess"] as! String
+                        }
+                    }
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.displayMyAlertMessage(mess)
+                    }
+                    
+                } catch {
+                    print(error)
+                }
+                
+            }
+            task.resume()
+            
         }
+        
     }
     
-
+    func generateBoundaryString() -> String {
+        return "Boundary-\(NSUUID().UUIDString)"
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    func createBodyWithParameters(parameters: [String: String]?,  boundary: String) -> NSData {
+        let body = NSMutableData();
+        
+        if parameters != nil {
+            for (key, value) in parameters! {
+                body.appendString("--\(boundary)\r\n")
+                body.appendString("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n")
+                body.appendString("\(value)\r\n")
+            }
+        }
+        
+        body.appendString("--\(boundary)--\r\n")
+        
+        return body
+    }
     
     func displayMyAlertMessage(userMessage:String){
-        let myAlert = UIAlertController(title: "Happ Message", message: userMessage, preferredStyle: UIAlertControllerStyle.Alert)
+        let myAlert = UIAlertController(title: "", message: userMessage, preferredStyle: UIAlertControllerStyle.Alert)
         let okAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil)
         myAlert.addAction(okAction)
         self.presentViewController(myAlert, animated: true, completion: nil)
