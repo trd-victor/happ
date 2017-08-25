9eabcdd14d370330ee33255ef25e2af84a1020fc
Reymond Bolambao <trd.rbolambao@gmail.com>
2017-08-25 19:11:31 +0800

    [update] added viewmessage

---
+++ happ/FirebaseSetup.swift
@@ -0,0 +1,9 @@
+//
+//  FirebaseSetup.swift
+//  happ
+//
+//  Created by TokikawaTeppei on 23/08/2017.
+//  Copyright © 2017 H-FUKUOKA. All rights reserved.
+//
+
+import Foundation
---
+++ happ/IncomingMessage.swift
@@ -0,0 +1,13 @@
+//
+//  IncomingMessage.swift
+//  happ
+//
+//  Created by TokikawaTeppei on 23/08/2017.
+//  Copyright © 2017 H-FUKUOKA. All rights reserved.
+//
+
+import UIKit
+
+class IncomingMessage: UICollectionViewCell {
+    
+}
