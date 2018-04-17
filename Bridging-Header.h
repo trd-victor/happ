41ff635e11a9d5d399bd8981ef5371b11ee23fb7
Ricsheil Cagay <trd.victora@gmail.com>
2018-04-17 11:03:44 +0800

    changed bridging header

--- Bridging-Header.h
+++ Bridging-Header.h
@@ -8,6 +8,6 @@
 
 #ifndef Bridging_Header_h
 #define Bridging_Header_h
-#import <JSQMessagesViewController/JSQMessages.h>
+//#import <JSQMessagesViewController/JSQMessages.h>
 
 #endif /* Bridging_Header_h */
